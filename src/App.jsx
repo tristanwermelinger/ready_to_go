a9653be02187ada832c2014d319978f8e8876a01
tristan <tristan.wermelinger@gmail.com>
2023-04-26 18:26:12 +0200

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [count, setCount] = useState(true);
 
-  return <></>;
-}
+  return (
+    <>
+      <div className="bloc">
+        <div>
+          <p>ON</p>
+          <p>ON</p>
+          <p>ON</p>
+        </div>
+        <div className="right">
+          <p>OFF</p>
+          <p>OFF</p>
+          <p>OFF</p>
+        </div>
+        <div className="bigred">
+          <span>NO WAY !</span>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
